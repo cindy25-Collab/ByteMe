1ce5154f735afcfe4a7c454c4698d56058bcc1b0
Samay Peralta <ikeay@plaiaundi.net>
2025-12-19 09:33:57 +0100

    El archivo esta terminado, es decir, cu codigo. Se le han insertado tanto arraylist como otras funciones, justo como pedia en este sprint

--- Proyecto_clase/src/Asignacion_de_clases_profesor.java
+++ Proyecto_clase/src/Asignacion_de_clases_profesor.java
@@ -1,10 +1,9 @@
-import java.util.ArrayList;
 import java.util.Random;
-
+import java.util.ArrayList;
 public class Asignacion_de_clases_profesor 
+
 {
-	//El siguiente codigo nos ayuda a asiganr de manera aleatoria una clase a un profesor.
-	//Para ello usamos las variables que privatizamos en la clase anterior
+	//el programa esta listo para enviarlo a git hub
 	
 	public void clase (registrar_profesor profesor)
 	{
@@ -12,15 +11,21 @@ public class Asignacion_de_clases_profesor
         String maestria2 = profesor.getMaestria2();
         String maestria3 = profesor.getMaestria3();
         String maestria4 = profesor.getMaestria4();
-        
-        //hola mundo
-        
-        //aqui han habido modificaciones
-        
-        ArrayList<String> textos = new ArrayList<>();
-        //he tenido que borrar el archivo para rehacerlo con arraylist
-        
 
+        ArrayList<String> textos = new ArrayList<>();
+
+        textos.add("CURSO DE EUSKERA DE NIVEL PRINCIPIANTE con apoyo de " 
+                   + maestria2 + maestria3 + maestria4);
+
+        textos.add("CURSO DE EUSKERA DE NIVEL INTERMEDIO" 
+                   + maestria2 + maestria3 + maestria4);
+
+        textos.add("CURSO DE EUSKERA DE NIVEL AVANZADO con apoyo de " 
+                   + maestria2 + maestria3 + maestria4);
+
+        Random rand = new Random();
+        int azar = rand.nextInt(textos.size()); // 0 1 2
+
+        System.out.println("Usted impartira clases en el: " + textos.get(azar));
     }
 }
-
